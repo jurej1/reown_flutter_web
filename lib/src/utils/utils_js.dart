c1e9c3397bea405cfd6ed028ec210ea29f651687
Jure Jures <jure.04jj@gmail.com>
2025-03-29 16:58:08 +0100

    Adding ether amount and utils_js

---
+++ lib/src/utils/utils_js.dart
@@ -0,0 +1,103 @@
+import 'dart:js_interop';
+import 'dart:js_util' as js_util;
+import 'dart:typed_data';
+
+import 'package:decimal/decimal.dart';
+import 'package:reown_flutter_web/src/js/reown.js.dart';
+
+class UtilsJS {
+  static Object? dartify(JSAny? jsObject, {bool deep = true}) {
+    if (jsObject == null) return null;
+    if (jsObject.isA<JSBigInt>()) return (jsObject as JSBigInt).toDart;
+    if (jsObject.isA<JSArray>()) return (jsObject as JSArray).toDartDynamicList;
+    if (jsObject.isA<JSObject>()) {
+      return deep ? (jsObject as JSObject).toMap() : jsObject;
+    }
+
+    return jsObject.dartify();
+  }
+
+  static JSAny? jsify(dynamic dartObject) {
+    // We do exhaustive types check here
+    // because `dartObject.jsify()` throws a "NoSuchMethodError: 'jsify'"
+    // whthout explicit cast.
+    if (dartObject == null) return null;
+    if (dartObject is BigInt) return dartObject.toJS;
+    if (dartObject is Iterable) return dartObject.toJSArray;
+    if (dartObject is String) return dartObject.toJS;
+    if (dartObject is int) return dartObject.toJS;
+    if (dartObject is bool) return dartObject.toJS;
+    if (dartObject is double) return dartObject.toJS;
+    if (dartObject is num) return dartObject.toJS;
+    if (dartObject is Int8List) return dartObject.toJS;
+    if (dartObject is Uint8List) return dartObject.toJS;
+    if (dartObject is Int16List) return dartObject.toJS;
+    if (dartObject is Uint16List) return dartObject.toJS;
+    if (dartObject is Uint16List) return dartObject.toJS;
+    if (dartObject is Int32List) return dartObject.toJS;
+    if (dartObject is Uint32List) return dartObject.toJS;
+    if (dartObject is Float32List) return dartObject.toJS;
+    if (dartObject is Float64List) return dartObject.toJS;
+    if (dartObject is List) return dartObject.toJSArray;
+    if (dartObject is Map) return js_util.jsify(dartObject);
+    return dartObject.jsify();
+  }
+}
+
+extension DartListToJS on Iterable {
+  JSArray<JSAny?> get toJSArray {
+    final jsArgs = JSArray<JSAny?>();
+    for (final arg in this) {
+      final jsValue = UtilsJS.jsify(arg as Object?);
+      jsArgs.push(jsValue);
+    }
+    return jsArgs;
+  }
+
+  JSArray<JSAny> get toNonNullableJSArray {
+    final jsArgs = JSArray<JSAny>();
+    for (final arg in this) {
+      final jsValue = UtilsJS.jsify(arg as Object?);
+      jsArgs.push(jsValue!);
+    }
+    return jsArgs;
+  }
+}
+
+extension JSBigIntArrayToList on JSArray<JSBigInt> {
+  List<BigInt> get toDartBigIntList =>
+      toDart.map((item) => item.toDart).toList();
+}
+
+extension JSNumberArrayToList on JSArray<JSNumber> {
+  List<Decimal> get toDartDecimalList =>
+      toDart.map((item) => Decimal.parse(item.toString())).toList();
+}
+
+extension JSAnyArrayToList on JSArray {
+  List<Object?> get toDartDynamicList => toDart.map(UtilsJS.dartify).toList();
+}
+
+extension JSObjectToMap on JSObject {
+  Map<String, dynamic> toMap({bool deep = true}) {
+    final map = <String, dynamic>{};
+
+    // Get the keys of the JSObject
+    final List<Object?> keys = js_util.callMethod(
+      js_util.getProperty(js_util.globalThis, 'Object'),
+      'keys',
+      [this],
+    );
+
+    // Iterate over the keys and assign values to the Dart map
+    if (keys.isEmpty) {
+      return map;
+    }
+    for (final key in keys) {
+      final keyString = key! as String;
+      final value = js_util.getProperty(this, keyString);
+      map[keyString] = UtilsJS.dartify(value, deep: deep);
+    }
+    return map;
+  }
+}

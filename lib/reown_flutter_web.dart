dec0fc056d38e16f31b662b59c1969362157761d
Jure Jures <jure.04jj@gmail.com>
2025-03-29 18:40:28 +0100

    adding old code.

--- lib/reown_flutter_web.dart
+++ lib/reown_flutter_web.dart
@@ -20,3 +20,46 @@ export 'src/reown_web3modal.dart';
 
 //   web.window.document.head!.appendChild(script);
 // }
+
+// OLD VERSION DOES NOT WORK
+// // THIS IS NOT USED AND IDK WHY NOT
+// /// Initializes the lib.
+// ///
+// /// This must be done before any interaction
+// /// with the lib.
+// Future<void> init() async {
+//   if (_isReady) return;
+
+//   final completer = Completer();
+
+//   _completeOnReadyEvent(completer);
+
+//   await _injectJavascriptModule('assets/main.js');
+
+//   _isReady = true;
+//   return completer.future;
+// }
+
+// void _completeOnReadyEvent(Completer completer) {
+//   const readyEventName = 'reown_flutter_web_ready';
+
+//   void readyEventListener(web.EventListener event) {
+//     web.window.removeEventListener(
+//       readyEventName,
+//       readyEventListener.toJS,
+//     );
+//     completer.complete();
+//   }
+
+//   web.window.addEventListener(readyEventName, readyEventListener.toJS);
+// }
+
+// Future<void> _injectJavascriptModule(String assetPath) async {
+//   final scriptPath = 'assets/packages/reown_flutter_web/$assetPath';
+
+//   final scriptNode = web.HTMLScriptElement()
+//     ..type = 'module'
+//     ..src = scriptPath;
+
+//   web.window.document.querySelector('html')!.appendChild(scriptNode);
+// }
